33c9be634d3c4c283f428bf8c5ee0187358fa9d9
luckycungwa <cungwalucky@gmail.com>
2023-08-03 13:54:22 +0200

    login auth is broken but functional

--- app.js
+++ app.js
@@ -1,66 +1,128 @@
-const express = require('express');
-const path = require('path');
-// import morgan
-const morgan = require("morgan");
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan"); // import morgan
 
 const app = express();
-// set up on port 3k or find available port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // set up on port 3k if not taken
 
 // Middleware
-// our predefined formats are : tiny, dev, short, common
-app.use(morgan("dev")); //'common' includes access date
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));    //Serves static files from the 'public' directory|| array
 
-// Use Firebase bd to manage users 
+app.use(morgan("dev")); // our predefined formats are : tiny, dev, short, common
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public"))); //Serves static files from the 'public' directory|| array
+
+// Use Firebase bd to manage users
 const users = [
-  { id: 1, username: 'user1@mlab.com', password: 'password1' },
-  { id: 2, username: 'user2@mlab.com', password: 'password2' },
+  { id: 1, username: "user1@mlab.com", password: "password1" },
+  { id: 2, username: "user2@mlab.com", password: "password2" },
 ];
 
-//MANAGE APP START & END as per assessment equest
-// Function to log 'start' and 'end' points'
-function logRunPoints() {
-    console.log("Started app...");
-    process.on("exit", () => {
-      console.log("Closed app...");
-    });
-  }
-  
+// i will log important events in during runtime instead of start & end
+function logRunPoints(event) {
+  console.log(`Event: ${event}`);
+}
+
+// Get user profile images for ARRAY
+function getUserImages(userId) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // STORING IMAGE IN AN ARRAY
+      const images = [
+        // accept png & jpg
+        "./img/image1.png",
+        "./img/image2.png",
+        "./img/image3.png",
+        "./img/image4.png",
+      ];
+      resolve(images);
+    }, 1500); //get images 1.5s
+  });
+}
+//   GET USER VIDEOS
+function getUserVideos(userId) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // STORE VIDEOS IN ARRAY
+      const videos = [
+        // formarts mp4, 3gp, avi etc
+        "video1.mp4",
+        "video2.3gp",
+        "video3.avi",
+        "video4.mov",
+      ];
+      resolve(videos);
+    }, 2000); //get images 2s (delayed 5ms after the images)
+  });
+}
+
+async function loadUserInfo() {
+  // Get user images after login
+  logRunPoints("Fetching images...");
+  const images = await getUserImages(users.id);
+  console.log("User images:", images);
+
+  // Get user videos 0.5s after images
+  // console.log("Loading videos...");
+  logRunPoints("Fetching videos...");
+  const videos = await getUserVideos(users.id);
+  console.log("User videos:", videos);
+
+  // console.log("USER PROFILE CREATED SUCCESSFUL!");
+  logRunPoints("USER PROFILE CREATED SUCCESSFUL!");
+}
+
 // Routes
-app.get('/', (req, res) => {
-  res.render('/home', { message: '' });
+app.get("/", (req, res) => {
+  res.render("home", { message: "CREATE PROFILE" });
 });
 
-// handle respomse and errors || 'log' action results
-app.post('/login', (req, res) => {
-    // 
+// MAIN LOGIN HANDLER || ASYNC with tryCatch error approacj
+app.post("/login", async (req, res) => {
+  try {
+    // start
+    // logRunPoints('Creating Profile');
+    const { email, password } = req.body; //get user data from the form body/fields
 
-  const { username, password } = req.body;
-//   validate credentials
-  const user = users.find(u => u.username === username && u.password === password);
+    // simulate async operation with timeout | db requests
+    await asyncTimer();
+    //   validate credentials within dummy users (must use Db later)
+    const user = users.find(
+      (u) => u.username === email && u.password === password
+    );
 
-  if (user) {
-    // send user to new success page
-    res.render('profile', { message: 'LOG IN SUCCESFUL!'});
-    // res.render('profile', { username });
-  } else {
-console.log()
-// return home/ refresh to try again
-    res.render('home', { message: 'Invalid credentials. Try Again!' });
+    // if credentials match send user to new success page or handle erros
+    if (email != "user1@mlab.com" && password != "password") {
+      logRunPoints("Login Successful");
+      // res.render('profile', { message: 'LOG IN SUCCESSFUL!', email: email });
+      loadUserInfo();
+    } else {
+      logRunPoints("Invalid credentials. Try Again!");
+      // res.render('home', { message: 'Login Failed. Try Again!' });
+      // logRunPoints(`username was: ${u.username}`);
+    }
+  } catch (error) {
+    logRunPoints("Error: Login failed!");
+    console.error(error);
+    // return home/ refresh to try again
+    // res.render('home', { message: 'An error occurred. Please try again later.' });
+    res.end();
   }
 });
+
+// setup timed delay using setTimeOut
+function asyncTimer() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 1000); // timed delay
+  });
+}
 
 // Set up EJS view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set("view engine", "ejs");  // set up view engine to ejs | 'res.render' loads the ejs view file
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs"); // set up view engine to ejs | 'res.render' loads the ejs view file
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}!`);
-   // Start loging the login process.
-      logRunPoints();
+  console.log(`Server is listening on port ${PORT}! http://localhost:${PORT}`);
+  // Start loging the login process.
+  logRunPoints("App Started");
 });
-
-
